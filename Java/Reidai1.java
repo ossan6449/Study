c1ad4e6d42498f75889ee089c483ac0c521acbe8
ossan <ossan6449@gmail.com>
2019-06-15 22:37:07 +0900

    2019.06.15-2

--- Java/Reidai1.java
+++ Java/Reidai1.java
@@ -1,11 +1,25 @@
-import ijava.awt.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class Reidai1{
     public static void main(String args[]){
-        MyFrame1 fm = new MyFrame1("フレームテスト");        // フレームオブジェクトを生成する
-        fm.setSize(200, 150);                               // ウィンドウサイズを横200、縦150にする
-        fm.setVisible(true);                                // フレーム（ウィンドウ）を表示する
-        fm.show();
+        MyFrame1 fm = new MyFrame1("フレームテスト");
+        fm.setSize(200, 150);
+        fm.setVisible(true);
+//      fm.show();
+    }
+}
+class MyFrame1 extends Frame{
+    MyFrame1(String title){
+        super(title);
+        addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        });
+    }
+
+    public void paint(Graphics g){
+        g.drawString("最初のフレームテストです",30,60);
     }
 }
